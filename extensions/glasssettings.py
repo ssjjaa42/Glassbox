fd15f297142c1d43001b8224b096b6dfc3ad5e9e
ssjjaa42 <stevie.antezana@gmail.com>
2023-05-08 13:51:15 -0400

    Implement $log and $unlog
    Glassbox will now track message edits and deletions in a certain channel, per server, with these commands.

--- extensions/glasssettings.py
+++ extensions/glasssettings.py
@@ -1,4 +1,5 @@
 # Image processing for Glassbox
+from datetime import datetime
 import discord
 from discord.ext import commands
 from modules.settings import *
@@ -32,6 +33,62 @@ class GlassSettings(commands.Cog):
             await ctx.send('This channel is now on the blacklist.')
         except IndexError as exception:
             await ctx.send(str(exception))
+    
+    @commands.command()
+    async def log(self, ctx: discord.ext.commands.Context):
+        """Tell the bot to record edited messages and deleted messages in the channel this command was invoked from."""
+        if not (ctx.message.author.guild_permissions.administrator or ctx.message.author.guild_permissions.manage_server):
+            await ctx.send('You need to have the Manage Server permission to change this setting.')
+            return
+        try:
+            add_logging(ctx.guild.id, ctx.channel.id)
+            await ctx.send('Now logging edits and deletions to this channel.')
+        except IndexError as exception:
+            await ctx.send(str(exception))
+
+    @commands.command()
+    async def unlog(self, ctx: discord.ext.commands.Context):
+        """Tell the bot to stop recording edits and deletions for this server."""
+        if not (ctx.message.author.guild_permissions.administrator or ctx.message.author.guild_permissions.manage_server):
+            await ctx.send('You need to have the Manage Server permission to change this setting.')
+            return
+        try:
+            clear_logging(ctx.guild.id)
+            await ctx.send('I\'m no longer logging edits and deletions in this server.')
+            await ctx.send('Hi No longer logging edits and deletions, I\'m--')
+        except IndexError as exception:
+            await ctx.send(str(exception))
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, before: discord.Message, after: discord.Message):
+        log_dict = logging()
+        if not str(after.guild.id) in log_dict:
+            return
+        embed = discord.Embed(title = 'Message Edit')
+        embed.description = f'[Link to message]({after.jump_url})'
+        embed.add_field(name = 'Channel', value = after.channel.mention, inline = False)
+        embed.add_field(name = 'Original Message', value = before.clean_content)
+        for attachment in before.attachments:
+            embed.add_field(name = f'Attachment: {attachment.filename}', value = attachment.url, inline = False)
+        embed.add_field(name = 'Edited Message', value = after.clean_content)
+        for attachment in after.attachments:
+            embed.add_field(name = f'Attachment: {attachment.filename}', value = attachment.url, inline = False)
+        embed.set_footer(text = f'{after.author.name}#{after.author.discriminator} • {after.edited_at.strftime("%m/%d/%Y %I:%M %p")}', icon_url = after.author.avatar.url)
+        await self.bot.get_channel(log_dict[str(after.guild.id)]).send(embed = embed)
+    
+    @commands.Cog.listener()
+    async def on_message_delete(self, message: discord.Message):
+        log_dict = logging()
+        if not str(message.guild.id) in log_dict:
+            return
+        embed = discord.Embed(title = 'Message Delete')
+        embed.add_field(name = 'Channel', value = message.channel.mention, inline = False)
+        embed.add_field(name = 'Original Message', value = message.clean_content)
+        for attachment in message.attachments:
+            embed.add_field(name = f'Attachment: {attachment.filename}', value = attachment.url, inline = False)
+        embed.set_footer(text = f'{message.author.name}#{message.author.discriminator} • {datetime.utcnow().strftime("%m/%d/%Y %I:%M %p")}', icon_url = message.author.avatar.url)
+        await self.bot.get_channel(log_dict[str(message.guild.id)]).send(embed = embed)
+        
 
 
 async def setup(bot):
--- modules/settings.py
+++ modules/settings.py
@@ -2,16 +2,21 @@
 import os
 import json
 
-forbidden_path = os.path.join(os.path.curdir, 'data', 'settings', 'forbidden.json')
+settings_path = os.path.join(os.path.curdir, 'data', 'settings')
+forbidden_path = os.path.join(settings_path, 'forbidden.json')
+logging_path = os.path.join(settings_path, 'logging.json')
+
+if not os.path.exists(settings_path):
+    os.mkdir(settings_path)
+if not os.path.exists(forbidden_path):
+    with open(forbidden_path, 'x') as f:
+        f.write('[]')
+if not os.path.exists(logging_path):
+    with open(logging_path, 'x') as f:
+        f.write('{}') # change to whatever an empty dictionary looks like
 
 
 def forbidden():
-    # Make the forbidden json if it doesn't exist yet
-    if not os.path.exists(os.path.join(os.path.curdir, 'data', 'settings')):
-        os.mkdir(os.path.join(os.path.curdir, 'data', 'settings'))
-    if not os.path.exists(forbidden_path):
-        with open(forbidden_path, 'x') as f:
-            f.write('[]')
     # Read the forbidden json
     with open(forbidden_path) as f:
         forbidden_ids = json.load(f)
@@ -38,3 +43,36 @@ def remove_forbidden(id: int):
     # Save the updated list
     with open(forbidden_path, 'w') as f:
         json.dump(forbidden_ids, f)
+
+
+def logging():
+    # Read the forbidden json
+    with open(logging_path) as f:
+        log_dict = json.load(f)
+    return log_dict
+
+
+def add_logging(guild: int, id: int):
+    log_dict = logging()
+    guild = str(guild)
+    if guild not in log_dict.keys():
+        log_dict[guild] = id
+    elif log_dict[guild] == id:
+        raise IndexError('Already logging this server to this channel.')
+    else:
+        log_dict[guild] = id
+    # Save the updated list
+    with open(logging_path, 'w') as f:
+        json.dump(log_dict, f)
+
+
+def clear_logging(guild: int):
+    log_dict = logging()
+    guild = str(guild)
+    if guild not in log_dict.keys():
+        raise IndexError('This server was not logged.')
+    else:
+        del log_dict[guild]
+    # Save the updated list
+    with open(logging_path, 'w') as f:
+        json.dump(log_dict, f)
