544f6f4f6200c090769231412485b8bc6701b2d2
ssjjaa42 <stevie.antezana@gmail.com>
2023-06-10 00:16:26 -0400

    Massively fix glasspictures
    $inspirational is now improved, and no longer can bring Glassbox to its
    knees as easily.
    
    $caption is a new command that simply adds a caption to the top of an
    image or gif.
    
    Code in pictures.py has been reorganized for efficiency and readability.
    
    All image processing commands can now handle gifs from the usual tenor
    links, and can also get their images from replied-to messages.
    
    	modified:   .gitignore
    	new file:   data/fonts/impact.ttf
    	modified:   extensions/glasspictures.py
    	modified:   modules/pictures.py

--- extensions/glasspictures.py
+++ extensions/glasspictures.py
@@ -12,36 +12,51 @@ class GlassPictures(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def _get_image(self, ctx: commands.Context, text=''):
+        if len(ctx.message.attachments) > 1:
+            raise LookupError('Too many images attached!')
+        elif len(ctx.message.attachments) == 1:
+            image_url = str(ctx.message.attachments[0])
+        elif len(ctx.message.attachments) == 0 and text.startswith('https://'):
+            words = text.split(' ')
+            image_url = words[0]
+            text = ' '.join(words[1:])
+        elif ctx.message.reference:
+            message = await ctx.channel.fetch_message(ctx.message.reference.message_id)
+            if len(message.attachments) > 1:
+                raise LookupError('Too many images attached!')
+            elif len(message.attachments) == 1:
+                image_url = str(message.attachments[0])
+            elif len(message.attachments) == 0 and message.content.startswith('https://'):
+                words = message.content.split(' ')
+                image_url = words[0]
+        else:
+            raise LookupError('No image provided!')
+        return (image_url, text)
+
     @commands.command()
-    async def downey(self, ctx: discord.ext.commands.Context, *, text=''):
+    async def downey(self, ctx: commands.Context, *, text=''):
         """Robert Downey, Jr. Explaining meme generator."""
         try:
-            pic_path = downey_meme(text)
             await ctx.message.delete()
+            await ctx.typing()
+            pic_path = downey_meme(text)
             await ctx.send(file=discord.File(pic_path))
         except UserWarning as exception:
             await ctx.send(str(exception))
 
     @commands.command()
-    async def inspirational(self, ctx: discord.ext.commands.Context, *, text=''):
+    async def inspirational(self, ctx: commands.Context, *, text=''):
         """Inspirational meme generator.
 
         Can take image inputs from an attachment or a direct URL at the start of the command invocation.
         A pipe character ('|') divides the top row of text from the bottom row.
         The bottom row of text (and the pipe character) is optional.
         """
-        if len(ctx.message.attachments) > 1:
-            await ctx.send('Too many images attached!')
-            return
-        elif len(ctx.message.attachments) == 1:
-            image_url = str(ctx.message.attachments[0])
-        elif len(ctx.message.attachments) == 0 and text.startswith('https://'):
-            words = text.split(' ')
-            image_url = words[0]
-            words.pop(0)
-            text = ' '.join(words)
-        else:
-            await ctx.send('No image provided!')
+        try:
+            image_url, text = await self._get_image(ctx, text)
+        except LookupError as exception:
+            await ctx.send(str(exception))
             return
         parts = text.split('|')
         header = parts[0].strip()
@@ -50,12 +65,32 @@ class GlassPictures(commands.Cog):
             body = parts[1].strip()
         try:
             await ctx.message.delete()
-            async with ctx.typing():
-                pic_path = inspirational_meme(image_url, header, body)
+            await ctx.typing()
+            pic_path = inspirational_meme(image_url, header, body)
             await ctx.send(file=discord.File(pic_path))
-        except UserWarning as exception:
+        except (UserWarning, LookupError) as exception:
             await ctx.send(str(exception))
+        
 
+    @commands.command()
+    async def caption(self, ctx: commands.Context, *, text=''):
+        """Image and gif caption maker.
+
+        Can take image inputs from an attachment or a direct URL at the start of the command invocation.
+        Tenor GIFs are also OK.
+        """
+        try:
+            image_url, text = await self._get_image(ctx, text)
+        except LookupError as exception:
+            await ctx.send(str(exception))
+            return
+        try:
+            await ctx.message.delete()
+            await ctx.typing()
+            pic_path = caption(image_url, text)
+            await ctx.send(file=discord.File(pic_path))
+        except (UserWarning, LookupError) as exception:
+            await ctx.send(str(exception))
 
 async def setup(bot):
     await bot.add_cog(GlassPictures(bot))
